12202c4161b3c1bb28e740af4e368e61ef999af0
Luiz Gustavo <luiz.guv@puccampinas.edu.br>
2026-05-11 19:09:12 -0300

    Exibição de auditoria implementada

--- menus/submenu/auditoria.py
+++ menus/submenu/auditoria.py
@@ -1,3 +1,7 @@
+# Esse é o auditoria.py do submenu, diferente do auditoria.py dos serviços
+
+from services.auditoria import listar_ocorrencias
+
 def auditoria():
     opcao = ""
 
@@ -5,9 +9,23 @@ def auditoria():
         while opcao != "0":
             print("\n=== AUDITORIA DA VOTAÇÃO ===")
             print("1 - Exibir Logs de Ocorrência")
-            print("2 - Exibit Protocolos de Votação")
+            print("2 - Exibir Protocolos de Votação")
             print("0 - Voltar")
 
             opcao = input("Escolha: ")
-    except:
-        print("Opção Inválida!")
+
+            if opcao == "1":
+                logs = listar_ocorrencias()
+                print("\n=== HISTÓRICO DE OCORRÊNCIAS ===")
+                
+                if logs:
+                    for log in logs:
+                        print(f"[{log['data_hora']}] {log['mensagem']}")
+                        
+                else:
+                    print("Nenhum registro encontrado no banco.")
+            
+            elif opcao == "0":
+                return
+    except Exception as e:
+        print(f"Erro ao carregar auditoria: {e}")
