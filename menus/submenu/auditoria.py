8b29cdbc29c570d61a449288a8e15d59fcbf3de9
Luiz Gustavo <luiz.guv@puccampinas.edu.br>
2026-05-12 11:05:42 -0300

    Refactor comments and clean up auditoria.py
    Updated comments for clarity and removed unused code.

--- menus/submenu/auditoria.py
+++ menus/submenu/auditoria.py
@@ -1,15 +1,14 @@
-# Esse é o auditoria.py do submenu, diferente do auditoria.py dos serviços
-
+# Esse é o auditoria.py do submenu (menus/submenus/auditoria.py), diferente do auditoria.py dos serviços (services/auditoria.py)
 from services.auditoria import listar_ocorrencias
 
 def auditoria():
     opcao = ""
 
-    try:
+    try:   
         while opcao != "0":
             print("\n=== AUDITORIA DA VOTAÇÃO ===")
             print("1 - Exibir Logs de Ocorrência")
-            print("2 - Exibir Protocolos de Votação")
+            #print("2 - Exibir Protocolos de Votação")
             print("0 - Voltar")
 
             opcao = input("Escolha: ")
@@ -24,6 +23,22 @@ def auditoria():
                         
                 else:
                     print("Nenhum registro encontrado no banco.")
+
+#            elif opcao == "2":
+#                print("\n=== PROTOCOLOS DE VOTAÇÃO ===")
+#                
+#                try:
+#                    from services.protocolo import listar_protocolos
+#                    protocolos = listar_protocolos()
+#
+#                    if protocolos:
+#                        for p in protocolos:
+#                            print(f"[{p['data_hora']}] ID: {p['id_voto']} - Eleitor: {p['eleitor']}")
+#                    else:
+#                        print("Nenhum protocolo encontrado.")#
+#
+#                except ImportError:
+#                    print("Serviço de protocolos ainda não implementado.")
             
             elif opcao == "0":
                 return
