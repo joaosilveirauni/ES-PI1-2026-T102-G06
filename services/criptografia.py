99d159e67ccbfa6381a62515e7d52b586e614a8a
João Pedro <joaopedrocva21@gmail.com>
2026-05-25 15:56:01 -0300

    finalizando criptografia

--- services/criptografia.py
+++ services/criptografia.py
@@ -1,14 +1,55 @@
+CHAVE = [
+    [1, 2, 3],
+    [0, 1, 4],
+    [5, 6, 0]
+]
+
+CHAVE_INVERSA = [
+    [2, 18, 5],
+    [20, 11, 22],
+    [21, 4, 1]
+]
+
 def cpf_para_vetor(cpf): # Transforma a string do cpf em uma lista de inteiros de 11 digitos
     return [int(d) for d in cpf]
 
-def adicionar_padding(vetor):
-    """
-    Transforma a lista do cpf em uma lista com 12 dígitos, pois trabalharemos com matriz 3x3
-    11 não é divisível por 3, portanto optamos por adicionar um 0 ao final do vetor
-    """
-    
-    vetor.append(0)
-    return vetor
 
-vetor = cpf_para_vetor("12345678900")
-print(adicionar_padding(vetor))
+
+def multiplicar_bloco(bloco):
+    resultado = [0, 0, 0]
+    resultado[0] = (CHAVE[0][0]*bloco[0] + CHAVE[0][1]*bloco[1] + CHAVE[0][2]*bloco[2]) % 26
+    resultado[1] = (CHAVE[1][0]*bloco[0] + CHAVE[1][1]*bloco[1] + CHAVE[1][2]*bloco[2]) % 26
+    resultado[2] = (CHAVE[2][0]*bloco[0] + CHAVE[2][1]*bloco[1] + CHAVE[2][2]*bloco[2]) % 26
+
+    return resultado
+
+def criptografar_cpf(cpf):
+    vetor = cpf_para_vetor(cpf)
+    vetor = vetor + [0]
+    cifrado = []
+    
+    for i in range(0, 12, 3):
+        bloco = vetor[i:i+3]
+        resultado = multiplicar_bloco(bloco)
+        cifrado = cifrado + resultado
+    
+    return cifrado
+
+
+def multiplicar_bloco_inverso(bloco):
+    resultado = [0, 0, 0]
+    resultado[0] = (CHAVE_INVERSA[0][0]*bloco[0] + CHAVE_INVERSA[0][1]*bloco[1] + CHAVE_INVERSA[0][2]*bloco[2]) % 26
+    resultado[1] = (CHAVE_INVERSA[1][0]*bloco[0] + CHAVE_INVERSA[1][1]*bloco[1] + CHAVE_INVERSA[1][2]*bloco[2]) % 26
+    resultado[2] = (CHAVE_INVERSA[2][0]*bloco[0] + CHAVE_INVERSA[2][1]*bloco[1] + CHAVE_INVERSA[2][2]*bloco[2]) % 26
+
+    return resultado
+
+def descriptografa_cpf(cifrado):
+    original =  []
+
+    for i in range(0, 12, 3):
+        bloco = cifrado[i:i+3]
+        resultado = multiplicar_bloco_inverso(bloco)
+        original = original + resultado
+    
+    return ''.join(str(n) for n in original[:11])
