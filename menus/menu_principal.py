a919970039d920a7a15aff6196686196aad46979
Luiz Gustavo <luiz.guv@puccampinas.edu.br>
2026-05-27 21:28:21 -0300

    Melhoria no Menu principal

--- menus/menu_principal.py
+++ menus/menu_principal.py
@@ -7,19 +7,19 @@ def menu_principal():
     while opcao != "0":
         try:
             print("\n=== MENU PRINCIPAL ===")
-            print("1 - Gerenciamento")
-            print("2 - Votação")
+            print("1 - Votar")
+            print("2 - Gerenciar")
             print("0 - Sair")
 
             opcao = input("Escolha: ")
 
             if opcao == "1":
-                menu_gerenciamento()
-            elif opcao == "2":
                 menu_votacao()
+            elif opcao == "2":
+                menu_gerenciamento()
             elif opcao == "0":
                 print("Saindo...")
             else:
-                print("Opção inválida!")
+                print("Opção Inválida!")
         except:
             print("Opção Inválida!")
